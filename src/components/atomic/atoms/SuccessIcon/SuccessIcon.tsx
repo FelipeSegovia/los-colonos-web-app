4f03cfc7713675b6bd2f60684ee5ea151945b49f
fsegovia <felipe.segovia.rod@gmail.com>
2024-09-25 17:02:44 -0300

    se eliminan declaraciones innecesarias

--- src/components/atomic/atoms/SuccessIcon/SuccessIcon.tsx
+++ src/components/atomic/atoms/SuccessIcon/SuccessIcon.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import "./SuccessIcon.scss"
 
-const SuccessIcon: React.FC = () => {
+const SuccessIcon = () => {
 
     return (<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" style={{padding: 0, margin: 0, verticalAlign: "sub"}} viewBox="0 0 24 24" ><path fill="#4fc685" d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"></path></svg>);
 }
